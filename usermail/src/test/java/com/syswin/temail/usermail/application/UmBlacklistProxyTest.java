9d1f259b1b0a1b1c780db08303b5dec1482ae809
李肖 <lixiao@syswin.com>
2019-06-13 10:43:05 +0800

    测试用例补充

--- usermail/src/test/java/com/syswin/temail/usermail/application/UmBlacklistProxyTest.java
+++ usermail/src/test/java/com/syswin/temail/usermail/application/UmBlacklistProxyTest.java
@@ -2,18 +2,20 @@ package com.syswin.temail.usermail.application;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.syswin.temail.usermail.core.exception.IllegalGmArgsException;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 public class UmBlacklistProxyTest {
 
+  UsermailBlacklistService usermailBlacklistService = Mockito
+      .mock(UsermailBlacklistService.class);
+  UmBlacklistProxy umBlacklistProxy = new UmBlacklistProxy(usermailBlacklistService);
   @Test
   public void checkInBlacklist() {
-    UsermailBlacklistService usermailBlacklistService = Mockito
-        .mock(UsermailBlacklistService.class);
-    UmBlacklistProxy umBlacklistProxy = new UmBlacklistProxy(usermailBlacklistService);
     String from = "from@syswin.com";
     String blacker = "to@syswin.com";
     umBlacklistProxy.checkInBlacklist(from, blacker);
@@ -23,5 +25,14 @@ public class UmBlacklistProxyTest {
         .isInBlacklist(argumentCaptor1.capture(), argumentCaptor2.capture());
     assertEquals(from, argumentCaptor1.getValue());
     assertEquals(blacker, argumentCaptor2.getValue());
+  }
+
+  @Test(expected = IllegalGmArgsException.class)
+  public void checkTemailIsInBlacklist(){
+     int count = 1;
+     String from = "from@syswin.com";
+     String blacker = "blacklist@syswin.com";
+     when(usermailBlacklistService.isInBlacklist(from,blacker)).thenReturn(count);
+     umBlacklistProxy.checkInBlacklist(from,blacker);
   }
 }
