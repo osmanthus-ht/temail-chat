89862c4cfa530a2f45eecb4aedc53e51502402fb
胡婷 <huting@syswin.com>
2019-07-05 10:39:28 +0800

    去掉未使用的注解

--- usermail/src/main/java/com/syswin/temail/usermail/UsermailAgentApplication.java
+++ usermail/src/main/java/com/syswin/temail/usermail/UsermailAgentApplication.java
@@ -29,9 +29,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @SpringBootApplication
 @ComponentScan(basePackages = "com.syswin.temail")
 public class UsermailAgentApplication extends SpringBootServletInitializer {
