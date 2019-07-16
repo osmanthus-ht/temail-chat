1580551871db8ad33f68b277967d04cb26f0cb58
申洋赫 <601334@syswin.com>
2019-07-16 16:20:21 +0800

    ReplyMongoMapper 增加注解

--- usermail-mongodb/src/main/java/com/syswin/temail/usermail/mongo/infrastructure/domain/UsermailReplyMongoMapper.java
+++ usermail-mongodb/src/main/java/com/syswin/temail/usermail/mongo/infrastructure/domain/UsermailReplyMongoMapper.java
@@ -26,7 +26,9 @@ package com.syswin.temail.usermail.mongo.infrastructure.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class UsermailReplyMongoMapper {
 
   @Autowired
